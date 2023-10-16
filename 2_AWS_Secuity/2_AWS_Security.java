f3c54c3eeb6c96ebfaba6127b9df4aaf0e137712
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-16 11:31:26 +0300

    Create 2_AWS_Security.java

---
+++ 2_AWS_Secuity/2_AWS_Security.java
@@ -0,0 +1,32 @@
+**Security-Related Services in AWS**
+
+Here are some security-related services in AWS:
+
+**1. IAM (Identity and Access Management):**
+- IAM is a service that enables you to manage access to AWS resources securely. It allows you to create and manage users, groups, and roles, and define their permissions to access AWS services and resources. IAM helps you enforce the principle of least privilege and ensure secure access control.
+
+**2. AWS STS (Security Token Service):**
+- STS is a web service that enables you to request temporary, limited-privilege credentials for IAM users or federated users. It helps you provide secure access to AWS resources without sharing long-term access keys. STS supports various authentication mechanisms, including federation with external identity providers.
+
+**3. AWS Config:**
+- AWS Config is a service that provides a detailed inventory of your AWS resources and records configuration changes over time. It helps you assess resource compliance against best practices and security standards. AWS Config enables you to monitor and evaluate the security and compliance of your AWS environment.
+
+**4. CloudWatch Logs:**
+- CloudWatch Logs is a service that enables you to monitor, store, and access log files from various AWS resources. It helps you centralize and analyze logs for troubleshooting, security analysis, and compliance auditing. CloudWatch Logs provides real-time insights into your application and infrastructure logs.
+
+**5. Amazon Elasticsearch Service:**
+- Elasticsearch Service is a fully managed service that makes it easy to deploy, operate, and scale Elasticsearch clusters in the AWS cloud. It allows you to search, analyze, and visualize your data in real-time. Elasticsearch Service provides security features like encryption, access control, and integration with AWS Identity and Access Management (IAM).
+
+**6. VPC (Virtual Private Cloud):**
+- VPC allows you to create a logically isolated virtual network within AWS. It provides control over IP addressing, subnets, routing, and security settings. VPC enables you to define network access control policies, configure security groups, and establish secure communication between resources.
+
+**7. Security Groups:**
+- Security Groups act as virtual firewalls for your EC2 instances and control inbound and outbound traffic. They allow you to define rules that permit or deny specific types of traffic based on protocols, ports, and IP addresses. Security Groups help you enforce network-level security and restrict access to your resources.
+
+**8. AWS Key Management Service (KMS):**
+- KMS is a managed service
+
+
+
+
+  ...
